452efce1046d3ad6a9eed89f6fd3f3fb6d2d5470
Akash Piya <akashpiya@gmail.com>
2024-02-03 18:54:41 -0600

    echor

--- tests/cli.rs
+++ tests/cli.rs
@@ -1,8 +1,19 @@
-use std::process::Command;
+use assert_cmd::Command;
 
 #[test]
 fn runs() {
-    let mut cmd = Command::new("hello");
-    let res = cmd.output();
-    assert!(res.is_ok());
+    let mut cmd = Command::cargo_bin("unix-commands-rust").unwrap();
+    cmd.assert().success().stdout("Hello, world!\n");
+}
+
+#[test]
+fn true_ok() {
+    let mut cmd = Command::cargo_bin("true").unwrap();
+    cmd.assert().success();
+}
+
+#[test]
+fn false_not_ok() {
+    let mut cmd = Command::cargo_bin("false").unwrap();
+    cmd.assert().failure();
 }
