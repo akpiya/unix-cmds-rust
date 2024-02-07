6aa24ebd97d26dded6551bcafc6b8073b406f18d
Akash Piya <akashpiya@gmail.com>
2024-02-07 10:45:59 -0600

    uniqr start

---
+++ uniqr/src/lib.rs
@@ -0,0 +1,20 @@
+use clap::{App, Arg};
+use std::error::Error;
+
+type MyResult<T> = Result<T, Box<dyn Error>>;
+
+#[derive(Debug)]
+pub struct Config {
+    in_file: String,
+    out_file: Option<String>,
+    count: bool,
+}
+
+pub fn get_args() -> MyResult<Config> {
+    let matches = A
+}
+
+pub fn run(config: Config) -> MyResult<()> {
+    println!("{:?}", config);
+    Ok(())
+}
